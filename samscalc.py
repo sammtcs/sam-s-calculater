9fe41da162a3653cdfe0ccc61dad84ca464bcec5
sammtcs <samm-farmington@thecoderschool.com>
2024-10-15 22:47:33 +0000

    make if statements that control what math problem make

--- samscalc.py
+++ samscalc.py
@@ -3,5 +3,18 @@ num2 = int(input("put your second number "))
 op = input("put the operoter(+,-,*,/):")
 print(num1, op, num2)
 
+if op == "+":
+    print(num1 + num2)
 
+elif op == "-":
+    print(num1 - num2)
+    
+elif op == ".":
+    print(num1 . num2)
+
+elif op == "*":
+    print(num1 * num2)
+
+elif op == "/":
+    print(num1 / num2)
 
